ed5f9ab3e7c777f9ff9a27feb1a0f98832074139
Ivan Galinskiy <ivanko.rus@gmail.com>
2018-03-16 22:26:45 +0100

    Functionality restored

--- m2i4931.py
+++ m2i4931.py
@@ -5,10 +5,17 @@ from __future__ import division
 
 import sys
 import numpy as np
-from scipy import signal
+from enum import Enum
 
 # import spectrum driver functions
 import pyspectrumdaq.Spectrum_M2i4931_pydriver.pyspcm as sp
+
+# Some useful constants
+
+# Terminations enum
+class Term(Enum):
+    TERM_50 = 0
+    TERM_1M = 1
 
 # Function for card name translation
 def szTypeToName (lCardType):
@@ -30,16 +37,34 @@ def szTypeToName (lCardType):
         sName = 'unknown type'
     return sName
 
-class connect():
+def chan_from_num(chan_n):
+    return getattr(sp, "CHANNEL{0:d}".format(int(chan_n)))
+
+class Card():
+    def _get32(self, param, uint=False):
+        if not uint:
+            destination = sp.int32(0)
+        else:
+            destination = sp.int32(0)
+        sp.spcm_dwGetParam_i32(self._hCard, param, sp.byref(destination))
+        return destination
+    
+    def _set32(self, param, val, uint=False):
+        val = int(val)
+        if not uint:
+            sp.spcm_dwSetParam_i32(self._hCard, param, sp.int32(val))
+        else:
+            sp.spcm_dwSetParam_i32(self._hCard, param, sp.uint32(val))
+    
     # Connect to DAQ card
     def __init__(self):
-        szErrorTextBuffer = sp.create_string_buffer (sp.ERRORTEXTLEN)
-        dwError = sp.uint32 ();
-        lStatus = sp.int32()
-        lAvailUser = sp.int32()
-        lPCPos = sp.int32()
-        qwTotalMem = sp.uint64(0);
-        qwToTransfer = sp.uint64(sp.MEGA_B(8));
+#        szErrorTextBuffer = sp.create_string_buffer (sp.ERRORTEXTLEN)
+#        dwError = sp.uint32 ();
+#        lStatus = sp.int32()
+#        lAvailUser = sp.int32()
+#        lPCPos = sp.int32()
+#        qwTotalMem = sp.uint64(0);
+#        qwToTransfer = sp.uint64(sp.MEGA_B(8));
         
         # open card
         self._hCard = sp.spcm_hOpen ("/dev/spcm0");
@@ -48,19 +73,16 @@ class connect():
             exit()
 
         # read type, function and sn and check for A/D card
-        lCardType = sp.int32(0)
-        sp.spcm_dwGetParam_i32 (self._hCard, sp.SPC_PCITYP, sp.byref(lCardType))
-        lSerialNumber = sp.int32(0)
-        sp.spcm_dwGetParam_i32 (self._hCard, sp.SPC_PCISERIALNO, sp.byref(lSerialNumber))
-        lFncType = sp.int32(0)
-        sp.spcm_dwGetParam_i32 (self._hCard, sp.SPC_FNCTYPE, sp.byref(lFncType))
+        lCardType = self._get32(sp.SPC_PCITYP)
+        lSerialNumber = self._get32(sp.SPC_PCISERIALNO)
+        lFncType = self._get32(sp.SPC_FNCTYPE)
 
-        sCardName = szTypeToName (lCardType.value)
+        sCardName = szTypeToName(lCardType.value)
         if lFncType.value == sp.SPCM_TYPE_AI:
-            sys.stdout.write("Found: {0} sn {1:05d}\n".format(sCardName,lSerialNumber.value))
+            print("Found: {0} sn {1:05d}\n".format(sCardName,lSerialNumber.value))
         else:
-            sys.stdout.write("Card: {0} sn {1:05d} not supported by example\n".format(sCardName,lSerialNumber.value))
-            exit ()
+            print("Card: {0} sn {1:05d} not supported by example\n".format(sCardName,lSerialNumber.value))
+            exit()
 
         # Acqusition and channel settings
         self.acquisition_set()
@@ -72,17 +94,48 @@ class connect():
     # Reset the card to default settings
     def reset(self):
         sp.spcm_dwSetParam_i32(self._hCard, sp.SPC_M2CMD, sp.M2CMD_CARD_RESET)
+        
+    # Initialize one channel (full range in Volts)
+    def ch_init(self, ch_n=1, termination=Term.TERM_1M, fullrange=10):
+        # Check that the channel number if correct
+        if ch_n not in range(4):
+            raise ValueError("The specified channel number is invalid")
+            
+        # Enable that channel
+        chan_param = getattr(sp, "CHANNEL{0:d}".format(int(ch_n)))
+        self._set32(sp.SPC_CHENABLE, chan_param)
+        
+        fullrange_val = int(fullrange * 1000)
+        if fullrange_val in [200,500,1000,2000,5000,10000]:
+            range_param = getattr(sp, "SPC_AMP{0:d}".format(int(ch_n)))
+            self._set32(range_param, fullrange_val);            
+            maxadc = self._get32(sp.SPC_MIINST_MAXADCVALUE)
+            self._maxadc = maxadc.value
+            self._conversion = float(fullrange_val)/1000 / self._maxadc
+        else:
+            raise ValueError("The specified voltage range is invalid")
+        
+        if termination == Term.TERM_1M:
+            term_val = 0
+        elif termination == Term.TERM_50:
+            term_val = 1
+        else:
+            raise ValueError("The specified termination is invalid")
+            
+        term_param = getattr(sp, "SPC_50OHM{0:d}".format(int(ch_n)))
+        self._set32(term_param, term_val)
 
     '''
     Specify memory length.
     Samplerate in Hz.
     Timeout in ms.
     Specify channel number as e.g. 0, 1, 2 or 3.
-    Fullrange is in mV has to be equal to one of {200,500,1000,2000,5000,10000}.
+    Fullrange is in V has to be equal to one of {0.2, 0.5, 1, 2, 5, 10}.
     Termination is equal to 1 for 50 Ohm and 0 for 1 MOhm
     '''            
     # Initializes acquisition settings
-    def acquisition_set(self, channel=1, memorylen=300e3, samplerate=30e6, timeout=10, fullrange=200, termination=1):
+    def acquisition_set(self, channel=1, memorylen=300e3, samplerate=30e6, 
+                        timeout=10, fullrange=10, termination=Term.TERM_1M):
         timeout *= 1e3 # Convert to ms
         self.memorylen = int(memorylen)
         self.samplerate = int(samplerate)
@@ -107,33 +160,13 @@ class connect():
         sp.spcm_dwSetParam_i64 (self._hCard, sp.SPC_SAMPLERATE,     sp.int64(self.samplerate));              # We want to have 30 MHz as sampling rate
 
         # Choose channel
-        g = globals()
-        if channel in [0,1,2,3]:
-            sp.spcm_dwSetParam_i32 (self._hCard, sp.SPC_CHENABLE,       g["CHANNEL" + str(channel)])
-        else:
-            raise ValueError("The specified channel is invalid")       
-
-        # Set channel range
-        if fullrange in [200,500,1000,2000,5000,10000]:
-            sp.spcm_dwSetParam_i32 (self._hCard, g["sp.SPC_AMP" + str(channel)], int(fullrange));            
-            maxadc = sp.int32(0)
-            sp.spcm_dwGetParam_i32(self._hCard, sp.SPC_MIINST_MAXADCVALUE, sp.byref(maxadc))
-            self._maxadc = maxadc.value
-            self._conversion = float(fullrange)/1000 / self._maxadc
-            #print("The conversion factor is {0:.2e} Volts per step".format(self._conversion))
-        else:
-            raise ValueError("The specified voltage range is invalid")
-
-        if termination in [0,1]:
-            sp.spcm_dwSetParam_i32 (self._hCard, g["sp.SPC_50OHM" + str(channel)], int(termination));
-        else:
-            raise ValueError("The specified input termination is invalid")
+        self.ch_init(channel, termination, fullrange)
 
         # define the data buffer
         # we try to use continuous memory if available and big enough
         self._pvBuffer = sp.c_void_p ()
         self._qwContBufLen = sp.uint64(0)
-        sp.spcm_dwGetContBuf_i64 (self._hCard, sp.spcm_BUF_DATA, sp.byref(self._pvBuffer), sp.byref(self._qwContBufLen))
+        sp.spcm_dwGetContBuf_i64 (self._hCard, sp.SPCM_BUF_DATA, sp.byref(self._pvBuffer), sp.byref(self._qwContBufLen))
         #sys.stdout.write ("ContBuf length: {0:d}\n".format(self._qwContBufLen.value))
         if self._qwContBufLen.value >= self._qwBufferSize.value:
             sys.stdout.write("Using continuous buffer\n")
@@ -141,7 +174,7 @@ class connect():
             self._pvBuffer = sp.create_string_buffer (self._qwBufferSize.value)
             #sys.stdout.write("Using buffer allocated by user program\n")
 
-        sp.spcm_dwDefTransfer_i64 (self._hCard, sp.spcm_BUF_DATA, sp.spcm_DIR_CARDTOPC, self._lNotifySize, self._pvBuffer, sp.uint64(0), self._qwBufferSize)
+        sp.spcm_dwDefTransfer_i64 (self._hCard, sp.SPCM_BUF_DATA, sp.SPCM_DIR_CARDTOPC, self._lNotifySize, self._pvBuffer, sp.uint64(0), self._qwBufferSize)
 
 
     '''
@@ -149,10 +182,10 @@ class connect():
     '''
     def acquire(self):
         # Reset buffer
-        sp.spcm_dwDefTransfer_i64 (self._hCard, sp.spcm_BUF_DATA, sp.spcm_DIR_CARDTOPC, self._lNotifySize, self._pvBuffer, sp.uint64(0), self._qwBufferSize)
+        sp.spcm_dwDefTransfer_i64 (self._hCard, sp.SPCM_BUF_DATA, sp.SPCM_DIR_CARDTOPC, self._lNotifySize, self._pvBuffer, sp.uint64(0), self._qwBufferSize)
 
         # start card and DMA
-        dwError = sp.spcm_dwSetParam_i32 (self._hCard, sp.M2CMD, sp.M2CMD_CARD_START |  sp.M2CMD_CARD_ENABLETRIGGER |  sp.M2CMD_DATA_STARTDMA)
+        dwError = sp.spcm_dwSetParam_i32 (self._hCard, sp.SPC_M2CMD, sp.M2CMD_CARD_START |  sp.M2CMD_CARD_ENABLETRIGGER |  sp.M2CMD_DATA_STARTDMA)
 
         # check for error
         szErrorTextBuffer = sp.create_string_buffer (sp.ERRORTEXTLEN)
@@ -164,7 +197,7 @@ class connect():
 
         # wait until acquisition has finished, then calculated min and max
         else:
-            dwError = sp.spcm_dwSetParam_i32 (self._hCard, sp.M2CMD,  sp.M2CMD_CARD_WAITREADY)
+            dwError = sp.spcm_dwSetParam_i32 (self._hCard, sp.SPC_M2CMD,  sp.M2CMD_CARD_WAITREADY)
             if dwError != sp.ERR_OK:
                 if dwError == sp.ERR_TIMEOUT:
                     sys.stdout.write ("... Timeout\n")
@@ -180,41 +213,11 @@ class connect():
 
                 return a
 
-    '''
-    Returns averaged power spectral density
-    '''
-    def PSD_avg(self,avg, progbar=False):
-        if progbar:            
-            from tqdm import tqdm
-            ran = tqdm(range(avg))
-        else:
-            ran = range(avg)
-            
-        for jj in ran:
-            # Get time trace
-            a = self.acquire()
-
-            # Calculate PSD
-            f, y = signal.periodogram(a, float(self.samplerate), 'hanning')
-
-            # Average PSD
-            if jj == 0:
-                psd = y
-            else:
-                psd += y
-        return f, psd/avg
-
-    # Get array for time
-    def time(self):
-        dt = 1/float(self.samplerate)
-        return np.linspace(0,self.memorylen*dt,self.memorylen)
-
 if __name__ == '__main__':
-    card = connect()
+    card = Card()
     card.acquisition_set(memorylen=30e6)#, samplerate=30e6, timeout=10e3, channel=1, fullrange=200, termination=1)
     for i in range(10):
         a = card.acquire()
         print(a[0])
-    t = card.time()
     card.close()
     

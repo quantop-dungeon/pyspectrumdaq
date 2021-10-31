004805d3b9ee6e7f7473e42eaba4a59630306a02
SAFedorov <fedorov.s.a@outlook.com>
2021-10-31 15:41:12 +0100

    Updates the setup file to include the sub-modules and the UI resources

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
 
 setup(
-    name="pyspectrumdaq",  
-    version="0.8.0",  
-    author="Ivan Galinskiy",  
-    packages=['pyspectrumdaq']
+    name="pyspectrumdaq",
+    version="0.8.0",
+    author="Ivan Galinskiy",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"": [os.path.join("rsc", "*")]}
 )
